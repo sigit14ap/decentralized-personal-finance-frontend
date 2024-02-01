3c2c192501e24a8b673ccd0fb8943a1ade42f550
sigit14ap <sigit1418@gmail.com>
2024-02-01 20:07:32 +0700

    feat(template): add flowbite template and remove unused code

---
+++ src/app/dashboard/content.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import type { NextPage } from "next";
+
+export const HomePageContent: NextPage = function () {
+  return (
+    <div className="p-6">
+      <section>
+        <header>
+          <h1 className="mb-6 text-5xl font-extrabold dark:text-white">
+            Dashboard
+          </h1>
+        </header>
+      </section>
+    </div>
+  );
+};
